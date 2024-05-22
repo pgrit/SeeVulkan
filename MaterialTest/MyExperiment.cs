14a3bde9aa8fa733c62664dce656dec594f9274a
Pascal Grittmann <grittmann@cg.uni-saarland.de>
2024-05-22 14:04:56 +0200

    restructure

---
+++ MaterialTest/MyExperiment.cs
@@ -0,0 +1,27 @@
+using System.Runtime.CompilerServices;
+
+namespace MaterialTest;
+
+class MyExperiment : Experiment {
+    public override List<Method> MakeMethods() => [
+        new("Path tracing", new PathTracer() { TotalSpp = 4 }),
+        new("VCM", new VertexConnectionAndMerging() { NumIterations = 2 })
+    ];
+
+    public override void OnDone(string workingDirectory, IEnumerable<string> sceneNames, IEnumerable<float> sceneExposures) {
+        // Run Python script to generate overview figure
+        try {
+            var scenes = sceneNames.Zip(sceneExposures).Select(kv => $"{kv.First};{kv.Second}");
+            string args = $"\"{workingDirectory}\" \"{string.Join(',', scenes)}\" \"{string.Join(',', MethodNames)}\"";
+            RunPythonScript("MakeFigure.py", args);
+        } catch(Exception) {
+            Logger.Warning("Running figuregen script with Python failed");
+        }
+    }
+
+    static void RunPythonScript(string scriptName, string arguments, [CallerFilePath] string callerPath = null) {
+        string scriptPath = Path.Join(Path.GetDirectoryName(callerPath), scriptName);
+        Logger.Log($"python \"{scriptPath}\" {arguments}");
+        Process.Start("python", $"{scriptPath} {arguments}").WaitForExit();
+    }
+}
